844bc28dba540a01ff1aafd82a05cb679a606d50
Christian Hernandez <christian_riverarivera@yahoo.com>
2023-08-27 15:55:38 -0600

    Set Project Files

---
+++ src/main/java/com/wallet/wallet/wallerwallet/WallerWalletApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.wallet.wallerwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallerWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallerWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallet/wallet/wallerwallet/WallerWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.wallet.wallerwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallerWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
